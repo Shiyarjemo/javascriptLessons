7c81e93e31b2fbc32c01122a1836e22e31b34e8c
Shiyar Jamo <1azadjemo@gmail.com>
2019-07-13 02:35:17 +0200

    Lessons

---
+++ جافا سكريبت/الدرس 7/جافا سكريبت/script.js
@@ -0,0 +1,13 @@
+// var car = {
+//     name:"BMW",
+//     model:"2019"
+//     ,color:"White"
+//             };
+var car = {};
+car.name = "BMW";
+car.model= 2019;
+
+document.getElementById("object").innerHTML= "Name: "+ car.name
++ " <br> Model: "+ car.model;
+
+
---
+++ جافا سكريبت/الدرس 9/جافا سكريبت/script.js
@@ -0,0 +1,7 @@
+var x = 2 ** 3;
+    x = Math.pow(2,3);
+
+
+var result = (2+3)*10;
+
+    document.getElementById("result").innerHTML = result;
